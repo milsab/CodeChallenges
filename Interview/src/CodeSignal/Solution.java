c51a35f071d428e8bdae96e88d99896f710a50ee
msabouri@hawk.iit.edu <milad.sabouri@gmail.com>
2019-09-21 23:00:03 -0500

    Common Character Counts

--- Interview/src/CodeSignal/Solution.java
+++ Interview/src/CodeSignal/Solution.java
@@ -1,6 +1,7 @@
 package CodeSignal;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Solution {
@@ -20,5 +21,29 @@ public class Solution {
             }
         }
         return ans.toArray(new String[]{});
+    }
+
+    int commonCharacterCount(String s1, String s2) {
+        if(s1.length() == 0 || s2.length() == 0)
+            return 0;
+
+        HashMap<Character, Integer> m1 = new HashMap<>();
+        HashMap<Character, Integer> m2 = new HashMap<>();
+        int ans = 0;
+
+        for(char c : s1.toCharArray()){
+            m1.put(c, m1.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c : s2.toCharArray()){
+            m2.put(c, m2.getOrDefault(c, 0) + 1);
+        }
+
+        for(char key : m1.keySet()){
+            if(m2.containsKey(key)){
+                ans += Math.min(m1.get(key), m2.get(key));
+            }
+        }
+        return ans;
     }
 }
